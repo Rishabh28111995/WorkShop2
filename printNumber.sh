9aee1a278a53d62ca749615c76a90be48a54321b
rishabhcenation <rishabhcenation28@gmail.com>
2023-03-07 09:00:26 +0530

    Add: Print numbers in series one to ten except few numbers.

--- printNumber.sh
+++ printNumber.sh
@@ -1,4 +1,12 @@
-for ((i=1; i<=10; i++))
-do
-   echo $i;
-done
+number=1;
+ 
+ while [ $number -le 10 ]
+ do
+    case $number in
+     1|2|4|7|9|10)
+
+     echo $number
+        ;;
+  esac
+    number=$((numberr++))
+ done
